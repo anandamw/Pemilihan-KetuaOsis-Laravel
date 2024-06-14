c233c73a1f45445fd43b7bd226ddee1860a0e57c
anandamw <ananda1850mw@gmail.com>
2024-06-14 13:00:35 +0700

    first commit

---
+++ resources/views/admin/layouts/template.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+    <!-- All Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="DexignLab">
+    <meta name="robots" content="">
+    <meta name="keywords"
+        content="bootstrap admin, card, clean, credit card, dashboard template, elegant, invoice, modern, money, transaction, Transfer money, user interface, wallet">
+    <meta name="description"
+        content="Dompet is a clean-coded, responsive HTML template that can be easily customised to fit the needs of various credit card and invoice, modern, creative, Transfer money, and other businesses.">
+    <meta property="og:title" content="Dompet - Payment Admin Dashboard Bootstrap Template">
+    <meta property="og:description"
+        content="Dompet is a clean-coded, responsive HTML template that can be easily customised to fit the needs of various credit card and invoice, modern, creative, Transfer money, and other businesses.">
+    <meta property="og:image" content="social-image.png">
+    <meta name="format-detection" content="telephone=no">
+
+    <!-- Mobile Specific -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('') }}assets/images/favicon.png">
+
+    <!-- Page Title Here -->
+    <title>Dompet - Payment Admin Dashboard Bootstrap Template</title>
+
+
+
+    <link href="{{ asset('') }}assets/vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('') }}assets/vendor/nouislider/nouislider.min.css">
+    <!-- Style css -->
+    <link href="{{ asset('') }}assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="waviy">
+            <span style="--i:1">L</span>
+            <span style="--i:2">o</span>
+            <span style="--i:3">a</span>
+            <span style="--i:4">d</span>
+            <span style="--i:5">i</span>
+            <span style="--i:6">n</span>
+            <span style="--i:7">g</span>
+            <span style="--i:8">.</span>
+            <span style="--i:9">.</span>
+            <span style="--i:10">.</span>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+
+
+
+
+    @yield('content')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--**********************************
+                Footer start
+            ***********************************-->
+    <div class="footer">
+
+        <div class="copyright">
+            <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">DexignLab</a>
+                2023</p>
+        </div>
+    </div>
+    <!--**********************************
+        Footer end
+       ***********************************-->
+
+
+
+
+    </div>
+    <!--**********************************
+       Main wrapper end
+      ***********************************-->
+
+    <!--**********************************
+       Scripts
+      ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{ asset('') }}assets/vendor/global/global.min.js"></script>
+    <script src="{{ asset('') }}assets/vendor/chart-js/chart.bundle.min.js"></script>
+    <script src="{{ asset('') }}assets/vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+
+    <!-- Apex Chart -->
+    <script src="{{ asset('') }}assets/vendor/apexchart/apexchart.js"></script>
+    <script src="{{ asset('') }}assets/vendor/nouislider/nouislider.min.js"></script>
+    <script src="{{ asset('') }}assets/vendor/wnumb/wNumb.js"></script>
+
+    <!-- Dashboard 1 -->
+    <script src="{{ asset('') }}assets/js/dashboard/dashboard-1.js"></script>
+
+    <script src="{{ asset('') }}assets/js/custom.min.js"></script>
+    <script src="{{ asset('') }}assets/js/dlabnav-init.js"></script>
+    <script src="{{ asset('') }}assets/js/demo.js"></script>
+    <script src="{{ asset('') }}assets/js/styleSwitcher.js"></script>
+
+</body>
+
+
+</html>
